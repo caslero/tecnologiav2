98e921b892eac1f13d9f710008d4dcbcdf966a28
carlo <carlosjperazab@gmail.com>
2023-04-26 12:22:49 -0400

    Guardado de los elementos del proyecto

---
+++ js/movilidad.js
@@ -0,0 +1,9 @@
+/** function Open(){
+    let ham = document.getElementById('hammm');
+    ham.classList.add('d-none');
+}
+
+function Close(){
+    let ham = document.getElementById('hammm');
+    ham.classList.remove('d-none');
+}**/
